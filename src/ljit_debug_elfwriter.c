31a7d95785b0e1cf1e987c36d19c9d47ef1e06f3
ruben_tytgat@hotmail.com <ruben_tytgat@hotmail.com@5afd4689-7d6f-472d-ca7f-e38a737ecd60>
2011-10-05 18:39:29 +0000

    git-svn-id: https://luajit-tilepro64.googlecode.com/svn/trunk@47 5afd4689-7d6f-472d-ca7f-e38a737ecd60

--- src/ljit_debug_elfwriter.c
+++ src/ljit_debug_elfwriter.c
@@ -2,7 +2,7 @@
 
 
 
-debug_elf* create_debug_elf(unsigned int buffer_size)
+debug_elf* debug_elf_create(unsigned int buffer_size)
 {
 	Elf *e;
 	Elf32_Ehdr *ehdr;
@@ -11,8 +11,11 @@ debug_elf* create_debug_elf(unsigned int buffer_size)
 
 
 	debug_elf *de = (debug_elf *)malloc(sizeof(debug_elf));
+
 	memset(de->strtab,0,sizeof(de->strtab));
 	de->strtab_len = 1;
+	memset(de->symtab,0,sizeof(de->symtab));
+	de->symtab_len = 0;
 
 	de->elf_data = (char*)malloc(sizeof(char)*buffer_size);
 	de->elf_file = fmemopen(de->elf_data,sizeof(char)*buffer_size,"w");
@@ -46,10 +49,52 @@ debug_elf* create_debug_elf(unsigned int buffer_size)
 
 	scn = elf_newscn(e);
 	shdr = elf32_getshdr(scn);
-	shdr->sh_name = 1;
+	shdr->sh_name = 11;
 	shdr->sh_type = SHT_STRTAB;
 	shdr->sh_flags = SHF_STRINGS | SHF_ALLOC;
 	shdr->sh_entsize = 0;
+	de->strtab_scn = scn;
 
+	scn = elf_newscn(e);
+	shdr = elf32_getshdr(scn);
+	shdr->sh_name = 19;
+	shdr->sh_type = SHT_SYMTAB;
+	shdr->sh_flags = SHF_ALLOC;
+	shdr->sh_entsize = 0;
+	shdr->sh_link = 1;
+	de->symtab_scn = scn;
+
+	return de;
 }
 
+void debug_elf_add_symbol(debug_elf* de, char* name, void* addr)
+{
+	Elf32_Sym* sym = &de->symtab[de->symtab_len++];
+	sym->st_name = de->strtab_len;
+	sym->st_value = addr;
+	sym->st_info = STT_FUNC;
+
+	strcpy(de->strtab+de->strtab_len,name);
+	de->strtab_len += strlen(name) + 1;
+}
+
+void debug_elf_finalize(debug_elf* de)
+{
+	Elf32_Shdr* shdr = elf32_getshdr(de->strtab_scn);
+	shdr->sh_entsize = de->symtab_len;
+
+	elf_update(de->elf,ELF_C_WRITE);
+}
+
+char* debug_elf_getaddr(debug_elf* de)
+{
+	return de->elf_data;
+}
+
+void debug_elf_release(debug_elf* de)
+{
+	elf_end(de->elf);
+	fclose(de->elf_file);
+	free(de->elf_data);
+	free(de);
+}
--- src/ljit_debug_elfwriter.h
+++ src/ljit_debug_elfwriter.h
@@ -13,6 +13,7 @@
 
 #define ELF_BUF_SIZE 20000
 #define ELF_STRTAB_SIZE 10000
+#define ELF_SYMTAB_SIZE 50
 
 
 typedef struct {
@@ -20,16 +21,28 @@ typedef struct {
 	char* elf_data;
 	FILE* elf_file;
 
+	Elf_Scn* strtab_scn;
+	Elf_Scn* symtab_scn;
+
 	const char shstrtab [] =
 	{
 			'\0',
 			'.','s','h','s','t',  'r','t','a','b','\0',
-			'.','s','y','m','t',  'a','b','\0',
-			'.','s','t','r','t',  'a','b','\0'
+			'.','s','t','r','t',  'a','b','\0',
+			'.','s','y','m','t',  'a','b','\0'
 	};
 
 	char strtab[ELF_STRTAB_SIZE];
 	unsigned int strtab_len;
+
+	Elf32_Sym symtab[ELF_SYMTAB_SIZE];
+	unsigned int symtab_len;
 } debug_elf;
+
+debug_elf* debug_elf_create(unsigned int buffer_size);
+void debug_elf_add_symbol(debug_elf* de, char* name, void* addr);
+void debug_elf_finalize(debug_elf* de);
+char* debug_elf_getaddr(debug_elf* de);
+void debug_elf_release(debug_elf* de);
 
 #endif /* LJIT_DEBUG_ELFWRITER_H_ */
